6e6efb8f752c12619a754459e88890895d59c587
Patautista <caleb.kart@gmail.com>
2023-05-29 13:46:51 -0300

    added .vs to repository

--- TrabCG/Window.h
+++ TrabCG/Window.h
@@ -3,12 +3,18 @@
 #include "PolygonFunctions.h"
 namespace lib {
 	class Window {
+	private:
+		Polygon m_window = Polygon({});
 	public:
-		Window(const Point start, Point end) : start(start), end(end) {
-			window = createRectangle(start, end);
+		Window(const Point& start, Point& end) : start(start), end(end){
+			auto temp = createRectangle(start, end);
+			m_window = *temp;
+			delete(temp);
 		}
-		Polygon* window;
 		Point start;
 		Point end;
+		void Translate(float x, float y) {
+			m_window.Translate(x, y, Point(0,0));
+		}
 	};
 }
